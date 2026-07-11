2ba7157f17b21d536cc82c70d2b1c55cf75ff19f
albertobzn9 <albertobzn9@gmail.com>
2026-07-11 14:13:30 -0600

    test(lab): persist launcher logs for supervised checks

--- tests/lab/Prueba_Audio_Estereo_RC3.bat
+++ tests/lab/Prueba_Audio_Estereo_RC3.bat
@@ -1,4 +1,4 @@
 @echo off
 set "ROOT=%~dp0CajaValentia_R2011a_RC3"
 cd /d "%ROOT%"
-"C:\Program Files (x86)\MATLAB\R2011a\bin\matlab.exe" -nosplash -r "cmc_prepara_entorno_r2011a; cmc_prueba_audio_estereo"
+"C:\Program Files (x86)\MATLAB\R2011a\bin\matlab.exe" -nosplash -logfile "%ROOT%\resultados\launcher_prueba_audio_estereo.txt" -r "cmc_prepara_entorno_r2011a; cmc_prueba_audio_estereo"
---
+++ tests/lab/Prueba_Sonido_Solo_10s_RC3.bat
@@ -0,0 +1,4 @@
+@echo off
+set "ROOT=%~dp0CajaValentia_R2011a_RC3"
+cd /d "%ROOT%"
+"C:\Program Files (x86)\MATLAB\R2011a\bin\matlab.exe" -nosplash -logfile "%ROOT%\resultados\launcher_prueba_sonido_solo.txt" -r "cmc_prepara_entorno_r2011a; cmc_prueba_fisica_sonido_solo_discriminacion"
