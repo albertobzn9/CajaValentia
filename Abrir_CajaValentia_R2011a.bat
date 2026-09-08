c8778e0480a6c21d876c161cbc13308576c14958
albertobzn9 <albertobzn9@gmail.com>
2026-09-07 18:28:01 -0600

    fix(launcher): use validated R2011a startup flow

--- Abrir_CajaValentia_R2011a.bat
+++ Abrir_CajaValentia_R2011a.bat
@@ -2,6 +2,12 @@
 setlocal
 
 rem Abre el menu completo de CajaValentia desde la raiz del repositorio.
+net session >nul 2>&1
+if not %errorlevel%==0 (
+    powershell -NoProfile -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+    exit /b
+)
+
 set "ROOT=%~dp0"
 set "MATLAB_DIR=%ROOT%matlab"
 set "MATLAB_EXE=C:\Program Files (x86)\MATLAB\R2011a\bin\matlab.exe"
@@ -26,6 +32,6 @@ if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
 if exist "%LOG_FILE%" del /q "%LOG_FILE%"
 
 cd /d "%MATLAB_DIR%"
-"%MATLAB_EXE%" -nosplash -logfile "%LOG_FILE%" -r "try, cmc_iniciar_gui_r2011a; catch err, disp(getReport(err)); end"
+"%MATLAB_EXE%" -nosplash -logfile "%LOG_FILE%" -r "cmc_iniciar_gui_r2011a"
 
 endlocal
